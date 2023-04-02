63c166d9cff5de590c35bf57b756126ba535092b
Ricky Salim <rickysalim81@gmail.com>
2023-04-02 10:20:14 +0700

    increment-day-5-2

--- asset/js/project.js
+++ asset/js/project.js
@@ -25,10 +25,10 @@ btnFile.addEventListener("change", function () {
 //   checkboxValue.map((item) => {
 //     document.getElementById("checkbox").innerHTML += `
 //      <div>
-//       <input 
-//         type="checkbox" 
-//         id="${item.name}" 
-//         value="${item.name}" 
+//       <input
+//         type="checkbox"
+//         id="${item.name}"
+//         value="${item.name}"
 //         class="${item.name}"
 //         style="padding-top: 0"
 //         />
@@ -45,16 +45,24 @@ function clearData() {
 
 function getData(projects) {
   clearData();
-  for (const project of projects) {
+  for (let i = 0; i < projects.length; i++) {
     document.getElementById("contents").innerHTML += `
         <div class="grid-item">
             <div class="project-item">
-                <img src="${project.image}" alt="Avatar" />
+                <img src="${projects[i].image}" alt="Avatar" />
                 <div class="subtitle">
-                    <h4>${project.project_name}</h4>
-                    <p>Durasi: ${getDuration(project.start_date, project.end_date)}</p>
-                    <p>${project.technologies}</p>
-                    <p>${project.description}</p>
+                    <h4>${projects[i].project_name}</h4>
+                    <p>Durasi: ${getDuration(
+                      projects[i].start_date,
+                      projects[i].end_date
+                    )}</p>
+                    <p></p>
+                    <p>${projects[i].description}</p>
+                </div>
+                <div class="tech-logo-section" id="tech-logo">
+                ${projects[i].technologies.map((item) => {
+                  return `${item?.image}`;
+                }).join(" ")}
                 </div>
                 <div class="action">
                     <button>Edit</button>
@@ -67,24 +75,24 @@ function getData(projects) {
 }
 
 function getDuration(startDate, endDate) {
-   const start = new Date(startDate)
-   const end = new Date(endDate)
-   const year = end.getFullYear() - start.getFullYear()
-   if(year > 0) {
-      return `${year} Tahun`
-   } else {
-      const month = (end.getMonth()+1) - (start.getMonth()+1)
-      if(month > 0) {
-        return `${month} Bulan`
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const year = end.getFullYear() - start.getFullYear();
+  if (year > 0) {
+    return `${year} Tahun`;
+  } else {
+    const month = end.getMonth() + 1 - (start.getMonth() + 1);
+    if (month > 0) {
+      return `${month} Bulan`;
+    } else {
+      const day = end.getDate() - start.getDate();
+      if (day > 0) {
+        return `${day} Hari`;
       } else {
-        const day = end.getDate() - start.getDate()
-        if(day > 0) {
-          return `${day} Hari`
-        } else {
-          return `Sehari`
-        }
+        return `Sehari`;
       }
-   }
+    }
+  }
 }
 
 function reset() {
@@ -102,18 +110,30 @@ function getProject(event) {
   const end_date = document.getElementById("end_date").value;
   const technologies = [];
 
-
   if (document.getElementById("node.js").checked) {
-    technologies.push(document.getElementById("node.js").value);
+    technologies.push({
+      name: document.getElementById("node.js").value,
+      image: '<img src="./asset/images/icons8-nodejs-48.png"/>',
+    });
   }
   if (document.getElementById("next.js").checked) {
-    technologies.push(document.getElementById("next.js").value);
+    technologies.push({
+      name: document.getElementById("next.js").value,
+      image: '<img src="./asset/images/react.png"/>',
+    });
   }
   if (document.getElementById("react.js").checked) {
-    technologies.push(document.getElementById("react.js").value);
+    technologies.push({
+      name: document.getElementById("react.js").value,
+      image: '<img src="./asset/images/next-js-icon-256x256-6j7ddke7.png"/>',
+    });
   }
+
   if (document.getElementById("typescript").checked) {
-    technologies.push(document.getElementById("typescript").value);
+    technologies.push({
+      name: document.getElementById("typescript").value,
+      image: '<img src="./asset/images/typescript.png"/>',
+    });
   }
   const image = document.getElementById("image-project").files[0];
   const blob = image ? URL.createObjectURL(image) : null;
@@ -121,25 +141,25 @@ function getProject(event) {
 
   // validation
 
-  if(!project_name) {
-    alert("Project Name Empty")
-    return false
-  } 
-  if(!start_date) {
-    alert("Start Date Empty")
-    return false
+  if (!project_name) {
+    alert("Project Name Empty");
+    return false;
   }
-  if(!end_date) {
-    alert("End Date Empty ")
-    return false
+  if (!start_date) {
+    alert("Start Date Empty");
+    return false;
   }
-  if(start_date > end_date){
-    alert("Start Date can't be Greater Than End Date")
-    return false
+  if (!end_date) {
+    alert("End Date Empty ");
+    return false;
   }
-  if(!description) {
-    alert("Description Empty")
-    return false
+  if (start_date > end_date) {
+    alert("Start Date can't be Greater Than End Date");
+    return false;
+  }
+  if (!description) {
+    alert("Description Empty");
+    return false;
   }
 
   let project = {
@@ -155,4 +175,3 @@ function getProject(event) {
   getData(projects);
   reset();
 }
-
